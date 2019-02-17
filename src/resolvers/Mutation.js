a679f9a1f987cb6b62293fd4ea112ec9a4fbc35b
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2019-02-18 01:26:06 +0200

    Module 3

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    console.log(ctx.db.mutation.createItem)
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
 
-module.exports = mutations;
+    console.log(item);
+
+    return item;
+  },
+};
+
+module.exports = Mutations;
